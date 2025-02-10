992d0642c68f80bdb8ba11aca746493aa087b22b
Lasha777-cyber <lashaghoniashvili3@gmail.com>
2025-02-11 01:49:39 +0400

    commit

---
+++ day 01/homework/lesson2.py
@@ -0,0 +1,8 @@
+name= "lasha"
+surname="ghoniashvili"
+age= 17
+height= 176
+love_cats= True
+is_old= False
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + str(love_cats) + " " + str(is_old) )
