1ce2298444e30070933214dfd103169cd9d774cd
Lasha777-cyber <lashaghoniashvili3@gmail.com>
2025-02-27 19:58:52 +0400

    commit

---
+++ day 02/homework/work1.py
@@ -0,0 +1,5 @@
+num1=25
+num2=35
+num3=29
+num4=67
+print(num1 + num2 + num3 + num4)
---
+++ day 02/homework/work2.py
@@ -0,0 +1,7 @@
+y=19
+y=int(18)
+y=int(input('enter number:'))
+if (y > 18):
+ print('x is positive')
+else:
+ print('x is positive')
