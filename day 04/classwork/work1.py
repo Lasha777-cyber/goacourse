c0ee9921a998e760e0a43fe33fc4fb511c675ff9
Lasha777-cyber <lashaghoniashvili3@gmail.com>
2025-03-07 21:55:28 +0400

    commit

---
+++ day 04/classwork/work1.py
@@ -0,0 +1,9 @@
+#making name input
+name='lasha'
+name=input("type your name: ")
+#making surname input
+surname='ghoniashvili'
+surname=input('type your surname: ')
+#making age input
+age=18
+age=input('type your age: ')
