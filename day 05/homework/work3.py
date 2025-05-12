2f2e26c7a274101860b46a6ca53a1da8715c2092
Lasha777-cyber <lashaghoniashvili3@gmail.com>
2025-05-12 21:33:30 +0400

    commit

---
+++ day 05/homework/work3.py
@@ -0,0 +1,7 @@
+num1=int(input("please enter first number: "))
+num2=int(input("please enter second number: "))
+num3=int(input("please enter third number: "))
+num4=int(input("please enter fourth number: "))
+num5=int(input("please enter fifth number: "))
+
+print((num1 + num2 + num3 + num4 + num5) /5)
---
+++ day 05/homework/work4.py
@@ -0,0 +1,5 @@
+celsius=int(input("please enter temperature in celsius: "))
+
+farenheit=(celsius * 9/5 + 32)
+
+print(farenheit)
